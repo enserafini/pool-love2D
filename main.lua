fb28c4698d3c7507912a248ff0dee697382e6fdd
Enzo <96983617+enserafini@users.noreply.github.com>
2026-08-30 00:03:06 -0300

    Definicion de respuestas a colisiones
    - Declara respuesta cuando inicia el contacto
    - Declara respuesta al terminar el contacto
    - Dibuja mensaje de prueba
    - Definicion de tags baranda y bocha

--- main.lua
+++ main.lua
@@ -2,33 +2,57 @@ baranda = {}
 baranda2 = {}
 bocha = {}
 
+entidad1 = nil
+entidad2 = nil
+contacto = false
+
+--Callbacks de colision - Funciones callback o de respuesta
+function iniciarContacto(a,b,col)
+    contacto = true
+    entidad1 = a:getUserData()
+    entidad2 = b:getUserData()
+end
+
+function terminarContacto(a,b,col)
+    contacto = false
+    entidad1 = nil
+    entidad2 = nil
+end
+
 
 function love.load()
     love.physics.setMeter(64)
     world = love.physics.newWorld(0, 9.81 * 64, true)
 
+    -- Se definen las funciones de callback para detectar colisiones
+    world:setCallbacks(iniciarContacto, terminarContacto)
+
 
     --los parametros de cuerpo significan:
     --tipo de cuerpo, posicion x, posicion y
     --si no definimos el tipo de cuerpo, por defecto es "static"
-    baranda.cuerpo = love.physics.newBody(world, 650/2, 650-25)
     
     --los parametros de forma significan:
     --ancho, alto
-    baranda.forma = love.physics.newRectangleShape(650, 50)
     
     --los parametros de acople significan:
     --cuerpo, forma
+
+    baranda.cuerpo = love.physics.newBody(world, 650/2, 650-25)
+    baranda.forma = love.physics.newRectangleShape(650, 50)
     baranda.acople = love.physics.newFixture(baranda.cuerpo, baranda.forma)
+    baranda.acople:setUserData("Baranda")
     
     baranda2.cuerpo = love.physics.newBody(world, 650/2, 25)
     baranda2.forma = love.physics.newRectangleShape(650, 50)
     baranda2.acople = love.physics.newFixture(baranda2.cuerpo, baranda2.forma)
+    baranda2.acople:setUserData("Baranda")
 
 
     bocha.cuerpo = love.physics.newBody(world, 650/2, 650/2, "dynamic")
     bocha.forma = love.physics.newCircleShape(20)   
     bocha.acople = love.physics.newFixture(bocha.cuerpo, bocha.forma)
+    bocha.acople:setUserData("Bocha")
     bocha.sprite = love.graphics.newImage("bola.png")
 
     love.window.setMode(650, 650)
@@ -45,7 +69,7 @@ end
 --"isrepeat" sirve para detectar si la tecla esta 
 --siendo presionada de manera continua, o si se presiono una sola vez
 function love.keypressed(key,scancode,isrepeat)
-    if key == "space" then
+    if key == "space" and contacto then
         bocha.cuerpo:applyLinearImpulse(0, -1000)
     end
 end
@@ -70,4 +94,12 @@ function love.draw()
                         0.075,
                         256,
                         256)
+                        
+    love.graphics.setColor(1,0,0)
+    if contacto then
+        love.graphics.print("CHOQUE", 650/2, 200 + 20)
+        love.graphics.print(entidad1, 650/2, 200 + 30)
+        love.graphics.print(entidad2, 650/2, 200 + 40)
+    end
+                        
 end
