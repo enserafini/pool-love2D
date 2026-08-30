c3381dc7b25f358025bf1d6706213a56e84b6714
Enzo <96983617+enserafini@users.noreply.github.com>
2026-08-29 23:32:57 -0300

    Programar primera bocha
    - Declaracion tabla bocha
    - Declaracion tabla baranda
    - Inicializacion cuerpo, forma y acople de bocha
    - Dibujo de bocha

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+baranda = {}
+bocha = {}
+
+
 function love.load()
     love.physics.setMeter(64)
     world = love.physics.newWorld(0, 9.81 * 64, true)
@@ -5,19 +9,37 @@ function love.load()
 
     --los parametros de cuerpo significan:
     --tipo de cuerpo, posicion x, posicion y
-    cuerpo = love.physics.newBody(world, 650/2, 650-25)
+    --si no definimos el tipo de cuerpo, por defecto es "static"
+    baranda.cuerpo = love.physics.newBody(world, 650/2, 650-25)
 
     --los parametros de forma significan:
     --ancho, alto
-    forma = love.physics.newRectangleShape(650, 50)
+    baranda.forma = love.physics.newRectangleShape(650, 50)
 
     --los parametros de acople significan:
     --cuerpo, forma
-    acople = love.physics.newFixture(cuerpo, forma)
+    baranda.acople = love.physics.newFixture(baranda.cuerpo, baranda.forma)
+
+    bocha.cuerpo = love.physics.newBody(world, 650/2, 650/2, "dynamic")
+    bocha.forma = love.physics.newCircleShape(20)   
+    bocha.acople = love.physics.newFixture(bocha.cuerpo, bocha.forma)
+    bocha.sprite = love.graphics.newImage("bola.png")
 
     love.window.setMode(650, 650)
 end
 
+function love.update(dt)
+    world:update(dt)
+end
+
 function love.draw()
-    love.graphics.polygon("fill", cuerpo:getWorldPoints(forma:getPoints()))
+    love.graphics.polygon("fill", baranda.cuerpo:getWorldPoints(baranda.forma:getPoints()))
+    love.graphics.draw( bocha.sprite,
+                        bocha.cuerpo:getX(),
+                        bocha.cuerpo:getY(),
+                        0,
+                        0.075,
+                        0.075,
+                        256,
+                        256)
 end
